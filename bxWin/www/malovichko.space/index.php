a0cfb8e749af3f9461c56c6d19c7bf34739ff7a8
outsideme <singsingingme@gmail.com>
2023-01-22 13:38:16 +0300

    step

--- bxWin/www/malovichko.space/index.php
+++ bxWin/www/malovichko.space/index.php
@@ -1,2 +1,3 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+Это главная!!!!
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- bxWin/www/malovichko.space/local/templates/bxWin/header.php
+++ bxWin/www/malovichko.space/local/templates/bxWin/header.php
@@ -25,3 +25,23 @@ $CurUri = $APPLICATION->GetCurUri();
 <?
 $APPLICATION->showPanel();
 ?>
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:menu", 
+	".default", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "top",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "N",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
